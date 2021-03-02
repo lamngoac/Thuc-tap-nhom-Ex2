f5b680fd2f20c385fb8e06c97dd5db56c40c0375
imtuuuuuu <vuanhtu111127@gmail.com>
2021-03-02 20:09:36 +0700

    form search hs

---
+++ Giao dien/SearchOut.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Giao_dien
+{
+    partial class SearchOut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgSearch = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgSearch
+            // 
+            this.dvgSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgSearch.Location = new System.Drawing.Point(32, 32);
+            this.dvgSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.dvgSearch.Name = "dvgSearch";
+            this.dvgSearch.RowHeadersWidth = 51;
+            this.dvgSearch.RowTemplate.Height = 24;
+            this.dvgSearch.Size = new System.Drawing.Size(526, 256);
+            this.dvgSearch.TabIndex = 1;
+            // 
+            // SearchOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 322);
+            this.Controls.Add(this.dvgSearch);
+            this.Name = "SearchOut";
+            this.Text = "SearchOut";
+            this.Load += new System.EventHandler(this.SearchOut_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgSearch)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgSearch;
+    }
+}
---
+++ Giao dien/SearchOut.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giao_dien
+{
+    public partial class SearchOut : Form
+    {
+        string sqlSearch;
+        public SearchOut()
+        {
+            InitializeComponent();
+        }
+
+        public SearchOut(string sql)
+        {
+            sqlSearch = sql;
+            InitializeComponent();
+        }
+
+
+        private void LoadDSHS()
+        {
+            dvgSearch.DataSource = null;
+            dvgSearch.DataSource = new CSDL().SelectData(sqlSearch);
+            dvgSearch.Columns["MaNV"].HeaderText = "Mã HS";
+            dvgSearch.Columns["TenNV"].HeaderText = "Tên HS";
+            dvgSearch.Columns["GioiTinh"].HeaderText = "Giới Tính";
+            dvgSearch.Columns["NgaySinh"].HeaderText = "Ngày Sinh";
+            dvgSearch.Columns["DiaChi"].HeaderText = "Địa Chỉ";
+            dvgSearch.Columns["MaLop"].HeaderText = "Mã Lớp";
+        }
+
+        private void SearchOut_Load(object sender, EventArgs e)
+        {
+            LoadDSHS();
+        }
+    }
+}
